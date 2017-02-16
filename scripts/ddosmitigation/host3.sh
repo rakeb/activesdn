91dc3f3852ffb1de17f3e0c962c15db5744c996c
Srini Seetharaman <seethara@stanford.edu>
2017-02-16 15:16:52 -0800

    ddos mitigation demo testing scripts seperated by each function

---
+++ scripts/ddosmitigation/host3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xterm -fg yellow -bg black -fa 'Monospace' -fs 12 -geometry 50x10+500+100 -e "echo 'H3 Pinging H10'; 
+ping -c75 10.0.0.10;
+sudo python generate_udp_traffic.py 10.0.0.3 10.0.0.10 200 22223;
+exit"
+#sudo python ~/Downloads/traffic_generator/generate_elephant_traffic.py 10.0.0.1 10.0.0.10 100 22222;
+#bash command if you want to stop xterm on a command line
